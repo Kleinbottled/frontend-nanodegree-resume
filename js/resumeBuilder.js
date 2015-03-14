fb342871c19e0324e15ab2fb70c86514694a8657
Kleinbottled <bitcoinbadger@hmamail.com>
2015-03-13 18:41:39 -0700

    resumebuilder.js updated with 4 variables and otherwise clean

---
+++ js/resumeBuilder.js
@@ -0,0 +1,107 @@
+
+
+//var name ="Brendan Drobek"
+//var formattedName = HTMLheaderName.replace("%data%", name);
+
+//var role1 ="Web Develpoer"
+//var formattedRole = HTMLheaderRole.replace("%data%", role1);
+
+//var skills = ["HTML", "CSS3", "Javascrpit", "bootsrap"];
+
+var bio = {
+	"topBio" : [
+	   {
+	   	"name" : "Brendan Drobek ",
+  		"role" : "Web Develpoer ",
+  		"pictureURL" : "http://placesheen.com/200/200",
+        "welcomeMessage" : "Hi there!",
+        "skills" : [
+  				"HTML", 
+  				"CSS3", 
+  				"Javascrpit", 
+  				"bootsrap"
+  				]
+  		}
+  		],
+  		   
+  	"contacts" : [
+  		{
+  		"mobile" : "555-555-9006",
+  		"email" : "Bitcoinbadger@Hmamail.com",
+  		"gitHubAddy" : "Kleinbottled"	
+  		}
+  	]
+}
+
+var work = {
+	"jobs" : [
+	  {
+	  	"employer" : "CVS Pharmacy",
+	  	"title" : "Assistant Manager",
+	  	"location" : "San Diego, CA, US",
+	  	"dates" : "April 2011, October 2014"
+	  },
+	  {
+	  	"employer" : "Various Restaurants",
+	  	"title" : "Pastry Chef, Cook",
+	  	"location" : "San Diego, CA, US",
+	  	"dates" : "early 2000, March 2011"	 
+	  }
+	]
+}
+
+var projects = {
+	"project" : [
+		{
+		  "title" : "Hello World!",
+		  "dates" : "March 5th 2015, March 7th 2015",
+		  "description" : "Learning a little HTML",
+		  "images" : [
+		  		"http://www.placecharlie.com/200/250",
+		  		"http://www.placecharlie.com/200/254"
+				]		
+		},
+		{
+		  "title" : "Portfolio",
+		  "dates" : "March 8th 2015, March 9th 2015",
+		  "description" : "Learning a little more HTML",
+		  "images" : [
+		  		"http://www.placecharlie.com/200/250",
+		  		"http://www.placecharlie.com/200/254"
+		 		]		
+		}
+	]	
+}
+
+var education = {
+	"schools": [
+	  {
+	  	"name" : "University City High School",
+	  	"city" : "San Diego, CA, US",
+	  	"degree" : "Graduated",
+	  	"major" : "none"
+	  },
+	  {	
+	    "name" : "Mesa Community College",
+	    "city" : "San Diego, CA, US",
+	    "degree" : "incomplete",
+	    "major" : "Sociology"
+      }
+    ],
+    "onlineEducation" : [
+      {
+      	"name" : "Udacity.com",
+      	"degree" : "Nanodegree",
+      	"major" : "Front-end Web Development"
+      }
+    ]
+}
+
+//$("#main").prepend(bio.name);
+//$("#main").append(bio.role);
+//$("#main").append(bio.contactInfo);
+//$("#picture").append(bio.pictureURL);
+//$("#main").append(bio.welcomeMessage);
+//$("#main").append(bio.skills);
+//$("#header").prepend(bio.role);
+
